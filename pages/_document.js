10497430c46d209e00c09c1a6828f977f048c302
Vaibhav Vinayak <vaibhav.vinayak@outlook.com>
2022-04-14 00:21:49 +0530

    added document component

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,13 @@
-import { Html } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return <Html lang="en"></Html>;
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
